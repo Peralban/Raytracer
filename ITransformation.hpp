2806f1d3dceb462d8430a1c4f558f3dc7e96f992
C0Florent <florent.charpentier@epitech.eu>
2024-04-30 15:51:36 +0200

    :sparkles: Added ITransformation interface

---
+++ ITransformation.hpp
@@ -0,0 +1,41 @@
+/*
+** EPITECH PROJECT, 2024
+** Raytracer
+** File description:
+** ITransformation
+*/
+
+#ifndef ITRANSFORMATION_HPP_
+    #define ITRANSFORMATION_HPP_
+
+    #include <optional>
+    #include "Point3D.hpp"
+
+namespace RayTracer {
+    class ITransformation {
+        public:
+            ITransformation() = default;
+            virtual ~ITransformation() = default;
+
+            /// @brief Get the transformed coordinates of a point
+            /// @param p The point to transform
+            /// @return The transformed point
+            virtual Math::Point3D transformCoordinates(const Math::Point3D &p) = 0;
+
+            /// @brief Undo a transformation
+            /// @param p The transformed point to untransform
+            /// @return If possible, the untransformed coordinates
+            ///
+            /// (Yes, some transformations are not reversible: for example, a
+            /// transformation that scales to 0 is not reversible since all points
+            /// are transformed to 0;0;0, thus it not possible where the point
+            /// was before the transformation from its 0;0;0 transformed coords)
+            virtual std::optional<Math::Point3D>
+            untransformCoordinates(const Math::Point3D &p) = 0;
+
+        protected:
+        private:
+    };
+}
+
+#endif /* !ITRANSFORMATION_HPP_ */
