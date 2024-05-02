feb984cddac137f404ae6272ba24d8d6d1da394a
Antoinegtir <antoine.gonthier@epitech.eu>
2024-05-02 17:39:04 +0200

    ✨ create obj class

---
+++ src/Scene/obj/Obj.cpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2024
+** Obj.cpp
+** File description:
+** DESCRIPTION
+*/
+
+#include "Obj.hpp"
+
+ObjPath::ObjPath()
+{
+    this->path = "";
+}
+
+ObjPath::~ObjPath()
+{
+}
---
+++ src/Scene/obj/Obj.hpp
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2024
+** Obj.hpp
+** File description:
+** DESCRIPTION
+*/
+
+#pragma once
+
+#include <string>
+#include <vector>
+
+class ObjPath {
+public:
+    std::string path;
+
+    ObjPath();
+    ~ObjPath();
+};
