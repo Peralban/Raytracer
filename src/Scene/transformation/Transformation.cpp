83afe1b17e4f71453d856804246956adbac78c49
Antoinegtir <antoine.gonthier@epitech.eu>
2024-05-02 17:39:24 +0200

    ✨ create Transformation class

---
+++ src/Scene/transformation/Transformation.cpp
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2024
+** Transformation.hpp
+** File description:
+** DESCRIPTION
+*/
+
+#include "Transformation.hpp"
+
+Transformation::Transformation()
+{
+    this->type = "";
+    this->value = {0, 0, 0};
+}
+
+Transformation::~Transformation()
+{
+}
+
---
+++ src/Scene/transformation/Transformation.hpp
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2024
+** Transformation.hpp
+** File description:
+** DESCRIPTION
+*/
+
+#pragma once
+
+#include <string>
+#include <vector>
+
+class Transformation {
+public:
+    std::string type;
+    std::vector<int> value;
+
+    Transformation();
+    ~Transformation();
+};
