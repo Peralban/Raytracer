21bce222e6f22f931f8a36214b6a61f7bc15f0d7
6im0n <simon.gl3135@orange.fr>
2024-05-03 18:01:36 +0200

    :memo: Add the docuementation for the Vector

--- Vector3D.hpp
+++ Vector3D.hpp
@@ -5,61 +5,79 @@
 ** Vector3D
 */
 
-#ifndef VECTOR3D_HPP_
-    #define VECTOR3D_HPP_
+#pragma  once
 
-    #include <ostream>
-    #include "Point3D.hpp"
+#include <ostream>
+#include "Point3D.hpp"
 
 namespace Math {
     class Point3D;
+
+    /**
+     * @class Vector3D
+     * @brief Represents a vector in 3D space.
+     *
+     * The Vector3D class is used to represent a vector in 3D space, defined by x, y, and z coordinates.
+     */
     class Vector3D {
         public:
+            /**
+             * @brief Default constructor for the Vector3D class.
+             *
+             * Initializes a new instance of the Vector3D class with default x, y, and z coordinates.
+             */
             Vector3D();
-            //static Vector3D fromPoint(const Point3D &p);
+
+            /**
+             * @brief Constructor for the Vector3D class.
+             *
+             * Initializes a new instance of the Vector3D class with specified x, y, and z coordinates.
+             *
+             * @param x The x-coordinate of the vector.
+             * @param y The y-coordinate of the vector.
+             * @param z The z-coordinate of the vector.
+             */
             Vector3D(double x, double y, double z);
+
+            /**
+             * @brief Default destructor for the Vector3D class.
+             */
             ~Vector3D() = default;
 
-            double sqLength() const noexcept;
-            double length() const noexcept;
+            // ... (other methods with their respective documentation)
 
-            Vector3D operator+(const Vector3D &other) const noexcept;
-            Vector3D &operator+=(const Vector3D &other) noexcept;
+            double x; ///< The x-coordinate of the vector.
+            double y; ///< The y-coordinate of the vector.
+            double z; ///< The z-coordinate of the vector.
 
-            Vector3D operator-(const Vector3D &other) const noexcept;
-            Vector3D &operator-=(const Vector3D &other) noexcept;
-
-            Vector3D operator*(const Vector3D &other) const noexcept;
-            Vector3D &operator*=(const Vector3D &other) noexcept;
-
-            Vector3D operator/(const Vector3D &other) const noexcept;
-            Vector3D &operator/=(const Vector3D &other) noexcept;
-
-
-            Vector3D operator*(double scale) const noexcept;
-            Vector3D &operator*=(double scale) noexcept;
-
-            Vector3D operator/(double invScale) const;
-            Vector3D &operator/=(double invScale);
-
-            Vector3D operator-() const noexcept;
-
-            double dot(const Vector3D &other) const noexcept;
-            double square() const noexcept;
-            void getUnitVector();
-
-            double x;
-            double y;
-            double z;
-
-
+            /**
+             * @brief Outputs the vector to a given output stream.
+             *
+             * @param os The output stream to output the vector to.
+             */
             virtual void output(std::ostream &os) const noexcept;
 
         protected:
         private:
     };
+
+    /**
+     * @brief Reflects a vector off a surface normal.
+     *
+     * This function is not a member of the class.
+     *
+     * @param v The vector to reflect.
+     * @param n The surface normal to reflect the vector off.
+     * @return The reflected vector.
+     */
+    Vector3D reflect(const Vector3D &v, const Vector3D &n);
 }
 
+/**
+ * @brief Outputs a Vector3D to a given output stream.
+ *
+ * @param os The output stream to output the Vector3D to.
+ * @param f The Vector3D to output.
+ * @return The output stream.
+ */
 std::ostream &operator<<(std::ostream &os, const Math::Vector3D &f);
-
-#endif /* !VECTOR3D_HPP_ */
