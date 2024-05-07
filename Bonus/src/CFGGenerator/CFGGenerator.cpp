73985d71be974ab4e8c6595c344056d7392a8cd7
PerAlban <alban.peralta@epitech.eu>
2024-05-07 11:19:28 +0200

    :zap: Delete useless std::cin in double

--- Bonus/src/CFGGenerator/CFGGenerator.cpp
+++ Bonus/src/CFGGenerator/CFGGenerator.cpp
@@ -71,16 +71,12 @@ void Bonus::CFGGenerator::askShape()
     bool enable_texture = false;
     std::vector<ParsingTransformation> transformations;
     std::cout << "Enter the type of the shape: ";
-    std::cin >> type;
     checkCin<std::string>(type, "Invalid type, please enter a valid type: ", []([[maybe_unused]]std::string ex) { return ex == "sphere" || ex == "cube" || ex == "cylinder" || ex == "cone"; });
-    std::cout << "Enter the position of the shape: ";
-    std::cin >> position[X] >> position[Y] >> position[Z];
+    std::cout << "Enter the position of the shape (first the X and enter, then the Y and enter, then the Z and enter):";
     checkCin<int, ALL_INDEXES>(position, "Invalid position, please enter a valid position for the position ", []([[maybe_unused]]int ex) { return true; }, A);
-    std::cout << "Enter the size of the shape: ";
-    std::cin >> size[X] >> size[Y] >> size[Z];
+    std::cout << "Enter the size of the shape (first the X and enter, then the Y and enter, then the Z and enter):";
     checkCin<int, ALL_INDEXES>(size, "Invalid size, please enter a valid size for the size ", []([[maybe_unused]]int ex) { return ex >= 0; }, A);
     std::cout << "Enable texture? (1 for yes, 0 for no): ";
-    std::cin >> enable_texture;
     checkCin<bool>(enable_texture, "Invalid input, please enter a valid input (1 for yes, 0 for no): ", []([[maybe_unused]]bool ex) { return ex == 0 || ex == 1; });
     if (enable_texture) {
         std::cout << "Enter the path to the texture of the shape: ";
@@ -90,20 +86,16 @@ void Bonus::CFGGenerator::askShape()
         path = "none";
     }
     std::cout << "Enable transformations? (1 for yes, 0 for no): ";
-    std::cin >> enable_transformation;
     checkCin<bool>(enable_transformation, "Invalid input, please enter a valid input (1 for yes, 0 for no): ", []([[maybe_unused]]bool ex) { return ex == 0 || ex == 1; });
     if (enable_transformation) {
-        std::cout << "Enter the number of transformations: ";
-        std::cin >> nb_transformations;
+        std::cout << "Enter the number of transformations (int): ";
         checkCin<int>(nb_transformations, "Invalid number of transformations, please enter a valid number of transformations: ", []([[maybe_unused]]int ex) { return ex > 0; });
         for (int i = 0; i < nb_transformations; i++)
             transformations.push_back(askTransformation());
     }
     std::cout << "Enter the type of the material: ";
-    std::cin >> material_type;
     checkCin<std::string>(material_type, "Invalid type, please enter a valid type: ", []([[maybe_unused]]std::string ex) { return ex == "matte" || ex == "metal" || ex == "glass"; });
-    std::cout << "Enter the color of the material: ";
-    std::cin >> color[R] >> color[G] >> color[B];
+    std::cout << "Enter the color of the material (first the R and enter, then the G and enter, then the B and enter):";
     checkCin<int, ALL_INDEXES>(color, "Invalid color, please enter a valid color for the color ", []([[maybe_unused]]int ex) { return ex >= 0 && ex <= 255; }, C);
     ParsingMaterial new_material(material_type, color[R], color[G], color[B]);
     ParsingShape new_shape(type, position[X], position[Y], position[Z], size[X], size[Y], size[Z], path, new_material, transformations);
@@ -120,29 +112,21 @@ void Bonus::CFGGenerator::askCamera()
     float aperture;
     float focus_dist;
     int resolution[RESO_INDEX];
-    std::cout << "Enter the position of the camera: ";
-    std::cin >> view_from[X] >> view_from[Y] >> view_from[Z];
+    std::cout << "Enter the position of the camera (first the X and enter, then the Y and enter, then the Z and enter): ";
     checkCin<int, ALL_INDEXES>(view_from, "Invalid view_from, please enter a valid integer for the position ", []([[maybe_unused]]int ex) { return true; }, A);
-    std::cout << "Enter the point the camera is looking at: ";
-    std::cin >> view_at[X] >> view_at[Y] >> view_at[Z];
+    std::cout << "Enter the point the camera is looking at (first the X and enter, then the Y and enter, then the Z and enter): ";
     checkCin<int, ALL_INDEXES>(view_at, "Invalid view_at, please enter a valid integer for the position ", []([[maybe_unused]]int ex) { return true; }, A);
-    std::cout << "Enter the up vector of the camera: ";
-    std::cin >> view_up[X] >> view_up[Y] >> view_up[Z];
+    std::cout << "Enter the up vector of the camera (first the X and enter, then the Y and enter, then the Z and enter): ";
     checkCin<int, ALL_INDEXES>(view_up, "Invalid view_up, please enter a valid integer for the position ", []([[maybe_unused]]int ex) { return true; }, A);
-    std::cout << "Enter the rotation of the camera: ";
-    std::cin >> rotation[X] >> rotation[Y] >> rotation[Z];
+    std::cout << "Enter the rotation of the camera (first the X and enter, then the Y and enter, then the Z and enter): ";
     checkCin<int, ALL_INDEXES>(rotation, "Invalid rotation, please enter a valid integer for the rotation ", []([[maybe_unused]]int ex) { return true; }, A);
-    std::cout << "Enter the field of view of the camera: ";
-    std::cin >> fov;
+    std::cout << "Enter the field of view of the camera (float): ";
     checkCin<float>(fov, "Invalid fov, please enter a valid field of view: ", []([[maybe_unused]]float ex) { return ex >= 0; });
-    std::cout << "Enter the aperture of the camera: ";
-    std::cin >> aperture;
+    std::cout << "Enter the aperture of the camera (float): ";
     checkCin<float>(aperture, "Invalid aperture, please enter a valid aperture: ", []([[maybe_unused]]float ex) { return ex >= 0; });
-    std::cout << "Enter the focus distance of the camera: ";
-    std::cin >> focus_dist;
+    std::cout << "Enter the focus distance of the camera (float): ";
     checkCin<float>(focus_dist, "Invalid focus distance, please enter a valid focus distance: ", []([[maybe_unused]]float ex) { return ex >= 0; });
-    std::cout << "Enter the resolution of the camera: ";
-    std::cin >> resolution[WIDTH] >> resolution[HEIGHT];
+    std::cout << "Enter the resolution of the camera (first the width and enter, then the height and enter): ";
     checkCin<int, RESO_INDEX>(resolution, "Invalid resolution, please enter a valid resolution for the ", []([[maybe_unused]]int ex) { return ex >= 0; }, RE);
     ParsingCamera new_camera(view_from[X], view_from[Y], view_from[Z],
     view_at[X], view_at[Y], view_at[Z], view_up[X], view_up[Y], view_up[Z],
@@ -181,14 +165,12 @@ void Bonus::CFGGenerator::askBackground()
     int color[ALL_INDEXES];
     bool enable_background = false;
     std::string path;
-    std::cout << "Enter the color of the background: ";
-    std::cin >> color[R] >> color[G] >> color[B];
+    std::cout << "Enter the color of the background (first the R and enter, then the G and enter, then the B and enter): ";
     checkCin<int, ALL_INDEXES>(color, "Invalid color, please enter a valid color for the color ", []([[maybe_unused]]int ex) { return ex >= 0 && ex <= 255; }, C);
-    std::cout << "Enable background? (1 for yes, 0 for no): ";
-    std::cin >> enable_background;
+    std::cout << "Enable background from texture file? (1 for yes, 0 for no): ";
     checkCin<bool>(enable_background, "Invalid input, please enter a valid input (1 for yes, 0 for no): ", []([[maybe_unused]]bool ex) { return ex == 0 || ex == 1; });
     if (enable_background) {
-        std::cout << "Enter the path of the background: ";
+        std::cout << "Enter the path of the background texture: ";
         std::cin >> path;
         checkCin<std::string>(path, "Invalid path, please enter a valid path: ", []([[maybe_unused]]std::string ex) { return std::filesystem::exists(ex); });
     } else {
@@ -205,20 +187,15 @@ void Bonus::CFGGenerator::askPrecision()
     bool enable_shadows = true;
     bool enable_reflections = true;
     bool enable_refractions = true;
-    std::cout << "Enter the number of samples: ";
-    std::cin >> samples;
+    std::cout << "Enter the number of samples (int): ";
     checkCin<int>(samples, "Invalid number of samples, please enter a valid number of samples: ", []([[maybe_unused]]int ex) { return ex > 0; });
-    std::cout << "Enter the number of bounces: ";
-    std::cin >> number_of_bounces;
+    std::cout << "Enter the number of bounces (int): ";
     checkCin<int>(number_of_bounces, "Invalid number of bounces, please enter a valid number of bounces: ", []([[maybe_unused]]int ex) { return ex > 0; });
     std::cout << "Enable shadows? (1 for yes, 0 for no): ";
-    std::cin >> enable_shadows;
     checkCin<bool>(enable_shadows, "Invalid input, please enter a valid input (1 for yes, 0 for no): ", []([[maybe_unused]]bool ex) { return ex == 0 || ex == 1; });
     std::cout << "Enable reflections? (1 for yes, 0 for no): ";
-    std::cin >> enable_reflections;
     checkCin<bool>(enable_reflections, "Invalid input, please enter a valid input (1 for yes, 0 for no): ", []([[maybe_unused]]bool ex) { return ex == 0 || ex == 1; });
     std::cout << "Enable refractions? (1 for yes, 0 for no): ";
-    std::cin >> enable_refractions;
     checkCin<bool>(enable_refractions, "Invalid input, please enter a valid input (1 for yes, 0 for no): ", []([[maybe_unused]]bool ex) { return ex == 0 || ex == 1; });
     ParsingPrecision new_precision(samples, number_of_bounces, enable_shadows, enable_reflections, enable_refractions);
     _precision = new_precision;
@@ -228,7 +205,6 @@ void Bonus::CFGGenerator::askObjFiles()
 {
     std::string path;
     std::cout << "Enter the path of the obj file: ";
-    std::cin >> path;
     checkCin<std::string>(path, "Invalid path, please enter a valid path: ", []([[maybe_unused]]std::string ex) { return std::filesystem::exists(ex); });
     _obj_files.push_back(path);
 }
@@ -237,11 +213,9 @@ Bonus::ParsingTransformation Bonus::CFGGenerator::askTransformation()
 {
     std::string type;
     int value[ALL_INDEXES];
-    std::cout << "Enter the type of the transformation: ";
-    std::cin >> type;
+    std::cout << "Enter the type of the transformation (translate, rotate, scale, mirror, shear): ";
     checkCin<std::string>(type, "Invalid type of transformation, please enter a valid type: ", []([[maybe_unused]]std::string ex) { return ex == "translate" || ex == "rotate" || ex == "scale" || ex == "mirror" || ex == "shear"; });
-    std::cout << "Enter the values of the transformation: ";
-    std::cin >> value[X] >> value[Y] >> value[Z];
+    std::cout << "Enter the values of the transformation (first the X and enter, then the Y and enter, then the Z and enter): ";
     checkCin<int, ALL_INDEXES>(value, "Invalid values, please enter a valid value for the values ", []([[maybe_unused]]int ex) { return ex >= 0; }, A);
     return ParsingTransformation(type, value[X], value[Y], value[Z]);
 }
