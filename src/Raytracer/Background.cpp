f3375ccf1217c46ef808996a9111de92518708b7
Antoine <antoine.gonthier@epitech.eu>
2024-05-07 11:41:14 +0200

    :bricks: create background

---
+++ src/Raytracer/Background.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2024
+** B-OOP-400-NAN-4-1-bsraytracer-florent.charpentier
+** File description:
+** Background
+*/
+
+#pragma once
+
+#include "Background.hpp"
+
+
+RayTracer::Background::Background() {}
+
+RayTracer::Background::~Background() {}
+
