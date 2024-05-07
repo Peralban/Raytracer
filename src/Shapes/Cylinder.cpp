faf8f734c06a14ae4aaf157969fd5979984223c7
Antoine <antoine.gonthier@epitech.eu>
2024-05-07 11:42:52 +0200

    :bricks: init cylinder shape (empty class)

--- src/Shapes/Cylinder.cpp
+++ src/Shapes/Cylinder.cpp
@@ -4,3 +4,17 @@
 ** File description:
 ** Cylinder
 */
+
+#include "Shapes/Cylinder.hpp"
+
+RayTracer::Cylinder::Cylinder()
+{
+
+}
+
+bool RayTracer::Cylinder::hit(const Math::Ray3D &ray, float tmin, float tmax, hits &hit) const
+{
+    return false;
+}
+
+RayTracer::Cylinder::~Cylinder() {}
--- src/Shapes/Cylinder.hpp
+++ src/Shapes/Cylinder.hpp
@@ -4,3 +4,22 @@
 ** File description:
 ** Cylinder
 */
+
+#pragma once
+
+#include <cmath>
+#include "Math/Vector3D.hpp"
+#include "Interfaces/IShape.hpp"
+
+namespace RayTracer {
+    class Cylinder : public IShape {
+        public:
+            Cylinder() = default;
+
+            Cylinder();
+
+            ~Cylinder() = default;
+
+            bool hit(const Math::Ray3D &ray, float tmin, float tmax, hits &hit) const override;
+    };
+}
