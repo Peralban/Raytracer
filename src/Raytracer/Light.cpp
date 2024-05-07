8a203a3074180173b3085b91d8b78bae6083115e
Antoine <antoine.gonthier@epitech.eu>
2024-05-07 11:41:21 +0200

    :bricks: create light

---
+++ src/Raytracer/Light.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2024
+** B-OOP-400-NAN-4-1-bsraytracer-florent.charpentier
+** File description:
+** Light
+*/
+
+#pragma once
+
+#include "Light.hpp"
+
+
+RayTracer::Light::Light() {}
+
+RayTracer::Light::~Light() {}
+
