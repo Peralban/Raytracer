58275422e70349f2830feff06aacec8a351d77c3
Antoine <antoine.gonthier@epitech.eu>
2024-05-09 00:31:42 +0200

    :fire: cleanify the main

--- src/main.cpp
+++ src/main.cpp
@@ -5,52 +5,16 @@
 ** DESCRIPTION
 */
 
-#include "Core/ClusterManagement.hpp"
-#include "Math/Vector3D.hpp"
-#include "Raytracer/Camera.hpp"
-#include "Shapes/ShapeList.hpp"
-#include "Shapes/Sphere.hpp"
-#include "Materials/Matte.hpp"
-#include "Materials/Metal.hpp"
-#include "Factory/Factory.hpp"
+#include "Core/Core.hpp"
 
-// int main()
-// {
-//     int width = 2160;
-//     int height = 1080;
-//     std::shared_ptr<RayTracer::ShapeList> scene = std::make_shared<RayTracer::ShapeList>();
-//     scene->shapes.push_back(std::make_shared<RayTracer::Sphere>(Math::Vector3D(0, 0, -1), 0.5, new RayTracer::Metal(Math::Vector3D(0.8, 0.3, 0.3), 0.3)));
-//     scene->shapes.push_back(std::make_shared<RayTracer::Sphere>(Math::Vector3D(0.0, -100.5, -1.0), 100, new RayTracer::Matte(Math::Vector3D(0.8, 0.8, 0.0))));
-//     scene->shapes.push_back(std::make_shared<RayTracer::Sphere>(Math::Vector3D(0.7, -0.3, -0.7), 0.2, new RayTracer::Metal(Math::Vector3D(0.8, 0.6, 0.2), 0.1)));
-//     scene->shapes.push_back(std::make_shared<RayTracer::Sphere>(Math::Vector3D(-1, 0, -1), 0.4, new RayTracer::Metal(Math::Vector3D(0.6, 0.3, 0.5), 1.0)));
-//     std::shared_ptr<RayTracer::Camera> camera = std::make_shared<RayTracer::Camera>(Math::Vector3D(-2, 2, 1), Math::Vector3D(0, 0, -1), Math::Vector3D(0, 1, 0), 90, double(width) / double(height), 0, 10.0);
-
-//     App::ClusterManagement clusterManagement(width, height);
-//     clusterManagement.executeRendering(scene, camera);
-//     clusterManagement.sortConfig();
-//     clusterManagement.createPPMFile();
-//     clusterManagement.initSDL();
-//     clusterManagement.printSDL();
-//     return 0;
-// }
-
-
-#include "DataRetriever/Parsing.hpp"
-#include <iostream>
-
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     try {
-        App::Parsing parser;
-        parser.checkArguments(argc, argv);
-
-        parser.parseConfigFile();
-
-        std::cout << parser << std::endl;
-
+        Core::Engine engine(argc, argv);
+        engine.run();
     } catch (const std::exception &e) {
         std::cerr << "Error: " << e.what() << std::endl;
         return 1;
     }
-
     return 0;
 }
