135ae70d55082fc862f4677dfbd6f4ed2f2f3f7c
Antoine <antoine.gonthier@epitech.eu>
2024-05-09 00:32:54 +0200

    :sparkles: implement the functions of the core

---
+++ src/Core/Core.cpp
@@ -0,0 +1,64 @@
+/*
+** EPITECH PROJECT, 2024
+** Core.cpp
+** File description:
+** Create the scene and camera, and render the scene.
+*/
+
+/**
+ * @file Core.cpp
+ * @brief Core class implementation.
+ * @see Core.hpp
+ */
+
+#include "Core.hpp"
+
+namespace Core {
+
+    Engine::Engine(int argc, char **argv) : _parser(), _factory(), _clusterManagement(2160, 1080) {
+        try {
+            _parser.checkArguments(argc, argv);
+            initialize();
+        } catch (const std::exception &e) {
+            std::cerr << "Error: " << e.what() << std::endl;
+        }
+    }
+
+    void Engine::run() {
+        render();
+        finalize();
+    }
+
+    void Engine::initialize() {
+        parseConfigFile();
+        createScene();
+        createCamera();
+    }
+
+    void Engine::parseConfigFile() {
+        _parser.parseConfigFile();
+    }
+
+    void Engine::createScene() {
+        _scene = std::make_shared<RayTracer::ShapeList>();
+        for (const auto& shapeData : _parser.getShapes()) {
+            auto shape = _factory.createPrimitive(shapeData);
+            _scene->shapes.push_back(shape);
+        }
+    }
+
+    void Engine::createCamera() {
+        auto cameraData = _parser.getCamera();
+        _camera = _factory.createCamera(cameraData);
+    }
+
+    void Engine::render() {
+        _clusterManagement.executeRendering(_scene, _camera);
+        _clusterManagement.sortConfig();
+        _clusterManagement.createPPMFile();
+        _clusterManagement.initSDL();
+        _clusterManagement.printSDL();
+    }
+
+    void Engine::finalize() {}
+}
