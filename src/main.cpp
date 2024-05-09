74affd4394fd02f2224def137d270675c5d2dc27
6im0n <simon.gl3135@orange.fr>
2024-05-09 18:38:54 +0200

    :zap: improve main, rotation cyliner

--- src/main.cpp
+++ src/main.cpp
@@ -27,26 +27,25 @@ int main()
     int width = 1920;
     int height = 1080;
     std::shared_ptr<RayTracer::ShapeList> scene = std::make_shared<RayTracer::ShapeList>();
-    scene->shapes.push_back(std::make_shared<RayTracer::Sphere>(Math::Vector3D(0.0, -100.5, -1.0), 100, std::make_shared<RayTracer::Matte>(Math::Vector3D(0.8, 0.8, 0.0))));
-    scene->shapes.push_back(std::make_shared<RayTracer::Sphere>(Math::Vector3D(0.7, -0.3, -0.7), 0.2, std::make_shared<RayTracer::Metal>(Math::Vector3D(0.8, 0.8, 0.8), 0.3)));
-    scene->shapes.push_back(std::make_shared<RayTracer::Sphere>(Math::Vector3D(-1, 0, -1), 0.4, std::make_shared<RayTracer::Glass>(1.1, Math::Vector3D(0.8, 0.3, 0.3))));
-    scene->shapes.push_back(std::make_shared<RayTracer::CylinderInfinite>(Math::Vector3D(0, 0, -1), 0.2, std::make_shared<RayTracer::Matte>(Math::Vector3D(0.8, 0.3, 0.3))));
-    scene->shapes.push_back(std::make_shared<RayTracer::CylinderLimited>(Math::Vector3D(1.0, 0.0, 1.0), 0.2, 0.2, std::make_shared<RayTracer::Matte>(Math::Vector3D(0.8, 0.3, 0.3))));
+    scene->shapes.push_back(std::make_shared<RayTracer::Sphere>(Math::Vector3D(0, -100, 0), 100, std::make_shared<RayTracer::Matte>(Math::Vector3D(0.8, 0.8, 0.8))));
+    //scene->shapes.push_back(std::make_shared<RayTracer::Sphere>(Math::Vector3D(0.7, -0.3, -0.7), 0.2, std::make_shared<RayTracer::Metal>(Math::Vector3D(0.8, 0.8, 0.8), 0.3)));
+    //scene->shapes.push_back(std::make_shared<RayTracer::Sphere>(Math::Vector3D(-1, 0, -1), 0.4, std::make_shared<RayTracer::Glass>(1.1, Math::Vector3D(0.8, 0.3, 0.3))));
+    //scene->shapes.push_back(std::make_shared<RayTracer::CylinderInfinite>(Math::Vector3D(0, 0, -1), 0.2, std::make_shared<RayTracer::Matte>(Math::Vector3D(0.8, 0.3, 0.3))));
+    //scene->shapes.push_back(std::make_shared<RayTracer::CylinderLimited>(Math::Vector3D(1.0, 0.0, 1.0), 0.2, 0.2, std::make_shared<RayTracer::Matte>(Math::Vector3D(0.8, 0.3, 0.3))));
 
     //std::unique_ptr<RayTracer::Rotation> rotation = std::make_unique<RayTracer::Rotation>(Math::Vector3D(1.2, 0.0, 0.0));
 
-    std::shared_ptr<RayTracer::Camera> camera = std::make_shared<RayTracer::Camera>(Math::Vector3D(.0, 2.0, 5.0), Math::Vector3D(0.0, 0.2, 0.0), Math::Vector3D(0.0, 0.2, 0.0), 45, double(width) / double(height), 0, 10.0);
     // scene->shapes.push_back(std::make_shared<RayTracer::Sphere>(Math::Vector3D{0, 0, 0}, 2., new RayTracer::Metal(Math::Vector3D(0.8, 0.3, 0.3), 0.3)));
-    scene->shapes.push_back(std::make_shared<RayTracer::Sphere>(Math::Vector3D{0, 0, 10}, 2., new RayTracer::Matte(Math::Vector3D(0.8, 0.8, 0.0))));
-    scene->shapes.push_back(std::make_shared<RayTracer::Sphere>(Math::Vector3D{10, 0, 0}, 4., new RayTracer::Metal(Math::Vector3D(0.8, 0.6, 0.2), 0.1)));
-    scene->shapes.push_back(std::make_shared<RayTracer::Sphere>(Math::Vector3D{10, 0, 10}, 2., new RayTracer::Metal(Math::Vector3D(0.6, 0.3, 0.5), 1.0)));
+    scene->shapes.push_back(std::make_shared<RayTracer::Sphere>(Math::Vector3D{0, 0, 10}, 2., std::make_shared<RayTracer::Matte>(Math::Vector3D(0.8, 0.3, 0.3))));
+    scene->shapes.push_back(std::make_shared<RayTracer::Sphere>(Math::Vector3D{10, 0, 0}, 4., std::make_shared<RayTracer::Metal>(Math::Vector3D(0.8, 0.3, 0.3), 0.3)));
+    scene->shapes.push_back(std::make_shared<RayTracer::Sphere>(Math::Vector3D{10, 0, 10}, 2., std::make_shared<RayTracer::Metal>(Math::Vector3D(0.8, 0.3, 0.3), 0.3)));
 
     scene->shapes.push_back(
         std::make_shared<RayTracer::TransformedShape>(
             std::make_unique<RayTracer::Sphere>(
                 Math::Vector3D(0, 0, 20),
                 2.,
-                new RayTracer::Matte(Math::Vector3D(0.8, 0.8, 0.0))
+                std::make_shared<RayTracer::Metal>(Math::Vector3D(0.8, 0.3, 0.3), 0.3)
             ),
             std::make_unique<RayTracer::Rotation>(
                 Math::Vector3D(0., 1.57, 0.)
@@ -55,13 +54,13 @@ int main()
     );
     scene->shapes.push_back(
         std::make_shared<RayTracer::TransformedShape>(
-            std::make_unique<RayTracer::Sphere>(
+            std::make_unique<RayTracer::CylinderInfinite>(
                 Math::Vector3D(10, 0, 10),
                 2.,
-                new RayTracer::Metal(Math::Vector3D(0.1, 1., 0.2), 0.1)
+               std::make_shared<RayTracer::Metal>(Math::Vector3D(0.8, 0.3, 0.3), 0.3)
             ),
-            std::make_unique<RayTracer::Scale>(
-                Math::Vector3D(2, 2, 2)
+            std::make_unique<RayTracer::Rotation>(
+                Math::Vector3D(0.0,0.0, 1.1)
             )
         )
     );
@@ -70,14 +69,14 @@ int main()
             std::make_unique<RayTracer::Sphere>(
                 Math::Vector3D(0, 0, 0),
                 4.,
-                new RayTracer::Metal(Math::Vector3D(0.7, 0.3, 0.8), 0.1)
+               std::make_shared<RayTracer::Metal>(Math::Vector3D(0.8, 0.3, 0.3), 0.3)
             ),
             std::make_unique<RayTracer::Shear>(
-                Math::Vector3D(0.2, 0., 0.1)
+                Math::Vector3D(0.8, 0., 0.1)
             )
         )
     );
-    std::shared_ptr<RayTracer::Camera> camera = std::make_shared<RayTracer::Camera>(Math::Vector3D(5, 20, -10), Math::Vector3D(5, 0, 5), Math::Vector3D(0, 1, 0), 90, double(width) / double(height), 0, 10.0);
+    std::shared_ptr<RayTracer::Camera> camera = std::make_shared<RayTracer::Camera>(Math::Vector3D(5, 20, -10), Math::Vector3D(5, 0, 5), Math::Vector3D(0, 1, 0), 90, double(width) / double(height), 0.0, 10.0);
     App::ClusterManagement clusterManagement(width, height);
     clusterManagement.executeRendering(scene, camera);
     clusterManagement.sortConfig();
