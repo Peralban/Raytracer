d522b055b41121b6aed5b195a2306fbecbde1320
PerAlban <alban.peralta@epitech.eu>
2024-05-12 20:11:14 +0200

    :bug: Fix plane shadows

--- src/Shapes/Plane.cpp
+++ src/Shapes/Plane.cpp
@@ -24,7 +24,7 @@ bool RayTracer::Plane::hit(const Math::Ray3D &ray, double tmin, double tmax, hit
     Math::Vector3D intersection_point = ray.getOrigin() + ray.getDirection() * t;
 
     // Check if the ray is hitting the plane from the opposite side
-    if (denom < 0) {
+    if (denom > 0) {
         hit.normal = -_normal; // Invert the normal
     } else {
         hit.normal = _normal;
